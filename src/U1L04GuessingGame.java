ca149e9c13978c723e19aa84fde23e3bb85ab6c7
antonia <antonia@172.16.21.76>
2018-09-27 12:31:51 -0400

    Guessing Game - attempts

--- src/U1L04GuessingGame.java
+++ src/U1L04GuessingGame.java
@@ -4,23 +4,13 @@ public class U1L04GuessingGame {
 
 	public static void main(String[] args) {
 		
-		//int computerNum = (int)(Math.random()* 100) + 1;
-		//do.. while loop  
-			//ask for user input “take a guess”
-					// convert to integer 
-			//if statement (num > computerNum) for too high 
-			//else if too low (num < computerNum)
-			//else number not in range 
-			//while loop for how many attempts after correctly 
-			//else how many attempts if incorrectly
-
 		// computer picks random number 
 		int computerNum = (int)(Math.random()* 100) + 1;
 		
 		int num = -1;
 		int guesses = 0;
 		// printer computers number
-		System.out.println("Computers number:  " + computerNum);
+		// System.out.println("Computers number:  " + computerNum);
 		
 		do {
 			// user inputs guess
@@ -30,23 +20,31 @@ public class U1L04GuessingGame {
 			if (num > 0 && num <= 100) {
 				guesses++;
 				
-			
 				// too high
 				if (num > computerNum) {
 					JOptionPane.showMessageDialog(null,"Too High !!");
-				}
-				
-				// to low 
+					}
+				// too low 
 				else if (num < computerNum) {
 					JOptionPane.showMessageDialog(null,"Too Low !!");
+					}
 				}
-			}
 				//if user goes out of the 1-100  
 				else {
 					JOptionPane.showMessageDialog(null,"Please pick a number between 1 and 100");
-				}
+					}
 		}while (num != computerNum || num == -1);
-		if (num == -1); 
 		
+		
+		if (num == 0) {
+		
+		// number attempts after giving up
+		JOptionPane.showMessageDialog(null,"You gave up after " + guesses + "attempts :-(");
+		}
+		// number f attempts after guessing correctly 
+		else {
+			JOptionPane.showMessageDialog(null, "You guessed it in " + guesses + " attempts :-)");
+					
+			}
 	}
 }
